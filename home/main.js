0fe4bc683eff42691f7948df6f4d5085fa0e5e6e
duck-vit <vietnguyen154022@gmail.com>
2025-07-13 10:11:03 +0700

    Add files via upload

---
+++ home/main.js
@@ -0,0 +1,149 @@
+document.getElementById("search_btn").onclick = function() {
+    var input = document.getElementById("input").value;
+  
+    // Video
+  
+    if(input.includes("big") & input.includes("tế") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/big-game-thực-tế-tập_1";
+    }
+    if(input.includes("big") & input.includes("tế") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/big-game-thực-tế-tập_2";
+    }
+    if(input.includes("big") & input.includes("tế") & input.includes("3")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/big-game-thực-tế-tập_2";
+    }
+    if(input.includes("big") & input.includes("tế") & input.includes("4")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/big-game-thực-tế-tập_2";
+    }
+    if(input.includes("shop") & input.includes("chí")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/buổi-shopping-của-chíp-ở-nhà-việt-và-ánh";
+    }
+    if(input.includes("chú") & input.includes("lượn") & input.includes("sầm")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/chúng-mình-đi-lượn-sầm-sơn";
+    }
+    if(input.includes("lịch") & input.includes("nha") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/du-lịch-nha-trang-tập_1";
+    }
+    if(input.includes("mini") & input.includes("gam") & input.includes("tế")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/mini-game-thực-tế";
+    }
+    if(input.includes("chủ") & input.includes("nhật") & input.includes("ánh")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/ngày-chủ-nhật-của-ánh";
+    }
+    if(input.includes("tết") & input.includes("2022") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/vlog-tết-nhâm-dần-2022-tập-1";
+    }
+    if(input.includes("tết") & input.includes("2022") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/vlog-tết-nhâm-dần-2022-tập-2";
+    }
+    if(input.includes("tết") & input.includes("2022") & input.includes("3")) {
+        window.location = "https://quill-groovy-editor.glitch.me/video/video_page/vlog-tết-nhâm-dần-2022-tập-3";
+    }
+  
+    // Short Story
+  
+    if(input.includes("uyện") & input.includes("mua")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/chuyện-mua-hàng";
+    }
+    if(input.includes("chiến") & input.includes("kết")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/cuộc-chiến-không-hồi-kết";
+    }
+    if(input.includes("kết") & input.includes("tặc") & input.includes("lam")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/kết-cục-của-ông-hải-tặc-tham-lam";
+    }
+    if(input.includes("miki") & input.includes("che") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/miki-&-cherry-tập_1";
+    }
+    if(input.includes("gấu") & input.includes("bông")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/những-chú-gấu-bông";
+    }
+    if(input.includes("live") & input.includes("chíp")) {
+        window.location = "https://quill-groovy-editor.glitch.me/short_story/short_story_page/buổi-livestream-bất-lực-của-chíp";
+    }
+  
+    // Film
+  
+    if(input.includes("cô") & input.includes("thuật") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cô-bé-phép-thuật-tập_1";
+    }
+    if(input.includes("cô") & input.includes("thuật") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cô-bé-phép-thuật-tập_2";
+    }
+    if(input.includes("cô") & input.includes("thuật") & input.includes("3")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cô-bé-phép-thuật-tập_3";
+    }
+    if(input.includes("gian") & input.includes("lận") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/đừng-gian-lận-tập_1";
+    }
+    if(input.includes("gian") & input.includes("lận") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/đừng-gian-lận-tập_2";
+    }
+    if(input.includes("gian") & input.includes("lận") & input.includes("3")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/đừng-gian-lận-tập_3";
+    }
+    if(input.includes("gian") & input.includes("lận") & input.includes("4")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/đừng-gian-lận-tập_4";
+    }
+    if(input.includes("gian") & input.includes("lận") & input.includes("5")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/đừng-gian-lận-tập_5";
+    }
+    if(input.includes("ngày") & input.includes("tuyệt") & input.includes("chúng")) {
+        if(input.includes("những") == true) {
+            if(input.includes("những") & input.includes("1")) {
+                window.location = "https://quill-groovy-editor.glitch.me/film/film_page/những-ngày-tuyệt-vời-của-chúng-tôi-tập_1";
+            }
+            if(input.includes("những") & input.includes("2")) {
+                window.location = "https://quill-groovy-editor.glitch.me/film/film_page/những-ngày-tuyệt-vời-của-chúng-tôi-tập_2";
+            }
+            if(input.includes("những") & input.includes("3")) {
+                window.location = "https://quill-groovy-editor.glitch.me/film/film_page/những-ngày-tuyệt-vời-của-chúng-tôi-tập_3";
+            }
+        }
+        if(input.includes("những") == false) {
+            if(input.includes("ngày") & input.includes("vời") & input.includes("1")) {
+                window.location = "https://quill-groovy-editor.glitch.me/film/film_page/ngày-tuyệt-vời-của-chúng-tôi-tập_1";
+            }
+            if(input.includes("ngày") & input.includes("vời") & input.includes("2")) {
+                window.location = "https://quill-groovy-editor.glitch.me/film/film_page/ngày-tuyệt-vời-của-chúng-tôi-tập_2";
+            }
+        }
+    }
+    if(input.includes("gia") & input.includes("phúc") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/gia-đình-hạnh-phúc-tập_1";
+    }
+    if(input.includes("gia") & input.includes("phúc") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/gia-đình-hạnh-phúc-tập_2";
+    }
+    if(input.includes("lớp") & input.includes("quậy")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/lớp-học-siêu-quậy";
+    }
+    if(input.includes("cô") & input.includes("thuật") & input.includes("mak")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cô-bé-phép-thuật-remake";
+    }
+    if(input.includes("thi") & input.includes("vẽ") & input.includes("1")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cuộc-thi-vẽ-tranh-tập_1";
+    }
+    if(input.includes("thi") & input.includes("vẽ") & input.includes("2")) {
+        window.location = "https://quill-groovy-editor.glitch.me/film/film_page/cuộc-thi-vẽ-tranh-tập_2";
+    }
+  
+  
+  
+  
+    if(input == "tabby-eager-quark.glitch.me") {
+        window.location = "https://tabby-eager-quark.glitch.me";
+    }
+    if(input == "hugy wase shop") {
+        window.location = "https://slow-poised-iodine.glitch.me";
+    }
+}
+
+document.getElementById("btn_show_search_content").onclick = function() {
+    document.getElementById("search_box").style.display = "block";
+    document.getElementById("btn_show_search_content").style.display = "none";
+}
+
+document.getElementById("btn_hide_search_content").onclick = function() {
+    document.getElementById("search_box").style.display = "none";
+    document.getElementById("btn_show_search_content").style.display = "block";
+}
---
+++ home/search_box/main.js
@@ -0,0 +1,9 @@
+document.getElementById("search_btn").onclick = function() {
+    var input = document.getElementById("input");
+    // var output = input.toLowerCase();
+  
+    if(input == "video") {
+        // window.location = "/sunstation_film/video";
+        alert("chào")
+    }
+}
